f14ac003ee70e5fe28902df357b812508c18531a
Yahiro D. Sakura <jcscuni5570@gmail.com>
2025-05-26 08:08:39 -0300

    feat: tela criada

---
+++ src/game.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+from config import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Game")
+        self.running = True
+
+    def play(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+
+        pygame.quit()
+
