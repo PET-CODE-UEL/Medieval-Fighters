51cf686b7bcc07cbb02c6301eae13d0eeebbf9cb
Yahiro D Sakura <jcscuuni5570@gmail.com>
2025-07-01 09:13:43 -0300

    BETA

--- src/lutador.py
+++ src/lutador.py
@@ -1,60 +1,220 @@
 import pygame
-from config import *
+from configuracoes import SPEED, LARGURA, GRAVIDADE, ALTURA, INDIGO, COOLDAWN
 
-
-class Lutador:
-    def __init__(self, x, y):
-        self.rect = pygame.Rect(x, y, 80, 180)
-        self.vel = 0
-        self.jump = False
-        self.attack_type = 0
-
-    def draw(self, surface):
-        pygame.draw.rect(surface, (0, 255, 0), self.rect)
-
-    def attack(self, surface):
-        attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, 2 * self.rect.width, self.rect.height)
-        pygame.draw.rect(surface, (0, 0, 255), attacking_rect)
-
-
-    def move(self, num, surface):
+class Lutador():
+    def __init__(self, player, x, y, flip, dados, sprite_sheet, animation_frame, som):
+        self.player = player
+        self.tamanho = dados[0]
+        self.imagem_escala = dados[1]
+        self.offset = dados[2]
+        self.virar = flip
+        self.animation_lista = self.carregando_Imgaens(sprite_sheet, animation_frame)
+        self.acao = 0 # 0 (ocioso), 1 (correr), 2 (pular), 3 (ataque 1), 4 (ataque 2), 5 (hit), 6 (morte)
+        self.frame_index = 0
+        self.imagem = self.animation_lista[self.acao][self.frame_index]
+        self.contador = pygame.time.get_ticks()
+        self.rect = pygame.Rect((x, y, 80, 180))
+        self.vel_y = 0
+        self.correr = False
+        self.pular = False
+        self.atacar = False
+        self.ataque_tipo = 0
+        self.tempo_de_ataque = 0
+        self.hit = False
+        self.vivo = True
+        self.som_ataque = som
+        self.vida = 200
+        
+    def carregando_Imgaens(self, spritesheet, animation_frame):
+        # Extraindo as imagens do spritesheet
+        animation_lista = []
+        for y, animation in enumerate(animation_frame):
+                
+            temp_imagem_lista = []
+            for x in range(animation):
+                temp_img = spritesheet.subsurface(x * self.tamanho, y * self.tamanho, self.tamanho, self.tamanho)
+                temp_imagem_lista.append(pygame.transform.scale(temp_img, (self.tamanho * self.imagem_escala, self.tamanho * self.imagem_escala)))
+            animation_lista.append(temp_imagem_lista)
+        return animation_lista
+            
+    def movimento(self, inimigo, RODADA):
         dx = 0
         dy = 0
-
-        # Pegando as teclas
+        self.correr = False
+        self.ataque_tipo = 0
+        
+        # Get keypress
         key = pygame.key.get_pressed()
-        if(num):
-            if(key[pygame.K_a]):
-                dx = -SPEED
-            if (key[pygame.K_d]):
-                dx = SPEED
-            # jump
-            if key[pygame.K_w] and not self.jump:
-                self.vel = -30
-                self.jump = True
-            # attack
-            if key[pygame.K_r] or key[pygame.K_f]:
-
-                # tipo de ataque
-                if key[pygame.K_r]:
-                    self.attak_type = 1
-                    self.attack(surface)
-                if key[pygame.K_f]:
-                    self.attak_type = 2
-
-        # Aplicando gravidade
-        self.vel += GRAVITY
-        dy += self.vel
-
-        # Pra n sair da tela
+        
+        # verificação de players
+        if self.player == 1:
+            
+            # só poderar executar as outras ações quando não estiver atacando
+            if self.atacar == False and self.vivo == True and RODADA == False:
+                
+                # Movimento
+                if key[pygame.K_a]:
+                    dx = -SPEED
+                    self.correr = True
+                if key[pygame.K_d]:
+                    dx = SPEED
+                    self.correr = True
+                
+                # Pulo
+                if key[pygame.K_w] and self.pular == False:
+                    self.vel_y = -30
+                    self.pular = True
+                    
+                # Ataques    
+                if key[pygame.K_r] or key[pygame.K_t]:
+                    self.Ataque(inimigo)
+                
+                    # Determinando o tipo de Ataque
+                    if key[pygame.K_r]:
+                        self.ataque_tipo = 1
+                    
+                    if key[pygame.K_t]:
+                        self.ataque_tipo = 2    
+                
+                # Visão dos Jogadores
+                if inimigo.rect.centerx > self.rect.centerx:
+                    self.virar = False
+                else:
+                    self.virar = True  
+                
+                # Aplicando o tempo de ataque no ataque
+                if self.tempo_de_ataque > 0:
+                    self.tempo_de_ataque -= 1          
+        
+        if self.player == 2:
+            
+            # só poderar executar as outras ações quando não estiver atacando
+            if self.atacar == False and self.vivo == True and RODADA == False:
+                
+                # Movimento
+                if key[pygame.K_LEFT]:
+                    dx = -SPEED
+                    self.correr = True
+                if key[pygame.K_RIGHT]:
+                    dx = SPEED
+                    self.correr = True
+                
+                # Pulo
+                if key[pygame.K_UP] and self.pular == False:
+                    self.vel_y = -30
+                    self.pular = True
+                    
+                # Ataques    
+                if key[pygame.K_KP1] or key[pygame.K_KP2]:
+                    self.Ataque(inimigo)
+                
+                    # Determinando o tipo de Ataque
+                    if key[pygame.K_KP1]:
+                        self.ataque_tipo = 1
+                    
+                    if key[pygame.K_KP2]:
+                        self.ataque_tipo = 2    
+                
+                # Visão dos Jogadores
+                if inimigo.rect.centerx > self.rect.centerx:
+                    self.virar = False
+                else:
+                    self.virar = True  
+                
+                # Aplicando o tempo de ataque no ataque
+                if self.tempo_de_ataque > 0:
+                    self.tempo_de_ataque -= 1  
+            
+            
+            
+            
+               
+        # Aplicando Gravidade
+        self.vel_y += GRAVIDADE
+        dy += self.vel_y
+            
+        # Limite de Tela
         if self.rect.left + dx < 0:
-            dx = 0 - self.rect.left
-        if self.rect.right + dx > SCREEN_WIDTH:
-            dx = SCREEN_WIDTH - self.rect.right
-        if self.rect.bottom + dy > SCREEN_HEIGHT - 80:
-            self.vel = 0
-            self.jump = False
-            dy = SCREEN_HEIGHT - 80 - self.rect.bottom
+            dx = -self.rect.left
 
+        if self.rect.right + dx > LARGURA:
+            dx = LARGURA - self.rect.right
+
+        if self.rect.bottom + dy > ALTURA - 80:
+            self.vel_y = 0
+            self.pular = False
+            dy = ALTURA - 80 - self.rect.bottom
+            
+            
+        # Atualizar movimebto
         self.rect.x += dx
         self.rect.y += dy
+        
+    # Atualizar Animações
+    def update(self):
+        # Verificação de Performace
+        if self.vida <= 0:
+            self.vida = 0
+            self.vivo = False
+            self.atualizar_Acao(6) # Morreu
+        elif self.hit == True:
+            self.atualizar_Acao(5) # Tomou dano
+        elif self.atacar == True:
+            if self.ataque_tipo == 1:
+                self.atualizar_Acao(3) # Ataque 1
+            elif self.ataque_tipo == 2:
+                self.atualizar_Acao(4) # Ataque 2
+        elif self.pular == True:
+            self.atualizar_Acao(2) # Pular
+        elif self.correr == True:
+            self.atualizar_Acao(1) # Correr
+        else:
+            self.atualizar_Acao(0) # Parado
+        
+        # Atualizando Imagem
+        self.imagem = self.animation_lista[self.acao][self.frame_index]
+        
+        # Verificando o temo de Atualização
+        if pygame.time.get_ticks() - self.contador > COOLDAWN:
+            self.frame_index += 1
+            self.contador = pygame.time.get_ticks()
+        # Verificação (fim da animação)
+        if self.frame_index >= len(self.animation_lista[self.acao]):
+            # Verificação se o jogador está morto
+            if self.vivo == False:
+                self.frame_index = len(self.animation_lista[self.acao]) - 1
+            else:
+                self.frame_index = 0
+                # Verificação de Execução de Ataques
+                if self.acao == 3 or self.acao == 4:
+                    self.atacar = False
+                    self.tempo_de_ataque = 40
+                # Verificação se foi levado Dano
+                if self. acao == 5:
+                    self.hit = False
+                    # Verificação se o jogador estava no meio de um ataque
+                    self.atacar = False
+                    self.tempo_de_ataque = 30
+        
+    def Ataque(self, inimigo):
+        if self.tempo_de_ataque == 0 and self.hit == False:
+            # Executando o Ataque
+            self.atacar = True
+            self.som_ataque.play()
+            attacking_rect = pygame.Rect(self.rect.centerx - (2.5 * self.rect.width * self.virar),
+                                        self.rect.y, 2.5 * self.rect.width, self.rect.height)
+            if attacking_rect.colliderect(inimigo.rect):
+                inimigo.vida -= 40
+                inimigo.hit = True
+        
+    
+    def atualizar_Acao(self, nova_acao):
+        if nova_acao != self.acao:
+            self.acao = nova_acao
+            # Atualizar configurações de Animações
+            self.frame_index = 0
+            self.contador = pygame.time.get_ticks()
+        
+    def draw(self, surface):
+        img = pygame.transform.flip(self.imagem, self.virar, False)
+        surface.blit(img, (self.rect.x - (self.offset[0] * self.imagem_escala), self.rect.y - (self.offset[1] * self.imagem_escala)))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,134 @@
-from game import *
+import pygame
+from pygame import mixer
+from configuracoes import *
+from lutador import Lutador
 
-if __name__ == "__main__":
-    game = Game()
-    game.play()
+mixer.init()
+pygame.init()
+
+# Criando a janela do jogo
+janela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Fighter")
+
+# Barra de Vida dos Lutadores
+def barra_vida(player, vida, x, y):
+    razao = vida / 200
+    pygame.draw.rect(janela, VERMELHO_ESCURO, (x - 5, y - 5, 410, 45))
+    pygame.draw.rect(janela, VERDE_ESCURO, (x, y, 400, 35))
+    pygame.draw.rect(janela, VERDE, (x, y, 400 * razao, 35))
+    
+# setando FPS
+clock = pygame.time.Clock()
+
+# Carregando Musicas e Sons
+pygame.mixer.music.load("suporte/audios/music.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(0, 0, 500)
+
+espada_fx = pygame.mixer.Sound("suporte/audios/sword.wav")
+espada_fx.set_volume(0.5)
+
+magia_fx = pygame.mixer.Sound("suporte/audios/magic.wav")
+magia_fx.set_volume(0.75)
+
+# Carregamento do Plano de Fundo
+PF_Imagem = pygame.image.load("suporte/imagens/background/florest.jpg").convert_alpha()
+
+# Carregando as Sprites
+cavaleiro_sheet = pygame.image.load("suporte/imagens/warrior/Sprites/warrior.png").convert_alpha()
+necromante_sheet = pygame.image.load("suporte/imagens/wizard/Sprites/wizard.png").convert_alpha()
+
+# Carregando a Vitoria
+vitoria_img = pygame.image.load("suporte/imagens/icons/victory.png").convert_alpha()
+
+# Definindo o n° de Frames por Animação
+CAVALEIRO_ANIMATION_FRAME = [10, 8, 1, 7, 7, 3, 7]
+NECROMANTE_ANIMATIO_FRAME = [8, 8, 1, 8, 8, 3, 7]
+
+# Definindo Fontes
+numeros_fonte = pygame.font.Font("suporte/fontes/turok.ttf", 100)
+pontuacao_fonte = pygame.font.Font("suporte/fontes/turok.ttf", 30)
+
+
+
+# Função para desenhar Textos
+def draw_Texto(texto, fonte, cor, x, y):
+    img = fonte.render(texto, True, cor)
+    janela.blit(img, (x, y))
+
+# Função para desenhar o Plano de Fundo
+def draw_pf():
+    escala_PF = pygame.transform.scale(PF_Imagem, (LARGURA, ALTURA)) # Convertendo o tamanho da imagem == a tamanho da janela
+    janela.blit(escala_PF, (0, 0))
+    
+# Criando Jogadores
+lutador_1 = Lutador(1, 200,310, False, NECROMANTE_DADOS, necromante_sheet, NECROMANTE_ANIMATIO_FRAME, magia_fx)
+lutador_2 = Lutador(2, 700,310, True, CAVALEIRO_DADOS, cavaleiro_sheet, CAVALEIRO_ANIMATION_FRAME, espada_fx)
+
+
+
+# Game Loop:
+while ATIVO:
+    clock.tick(FPS)
+    # Plano de Fundo
+    draw_pf()
+    
+    # Mostrando a barra de Vida
+    barra_vida(lutador_1.player ,lutador_1.vida, 15, 15)
+    barra_vida(lutador_2.player ,lutador_2.vida, 580, 15)
+    draw_Texto("P1:  " + str(PONTUAÇÃO[0]), pontuacao_fonte, VERMELHO_ESCURO, 20, 60)
+    draw_Texto("P2:  " + str(PONTUAÇÃO[1]), pontuacao_fonte, VERMELHO_ESCURO, 580, 60)    
+    if INICIO_CONTADOR <= 0:
+        # movimento dos Lutadores
+        lutador_1.movimento(lutador_2, RODADA)
+        lutador_2.movimento(lutador_1, RODADA)
+    else:
+        # Exibição do Contador de Tempo
+        draw_Texto(str(INICIO_CONTADOR), numeros_fonte, VERMELHO, LARGURA/2, ALTURA/3)
+        
+        # Atualização do Contador de Tempo
+        if (pygame.time.get_ticks() - ULTIMA_NUMERO) >= 1000:
+            INICIO_CONTADOR -= 1
+            ULTIMA_NUMERO = pygame.time.get_ticks()
+
+    # Animando Lutadores
+    lutador_1.update()
+    lutador_2.update()    
+    
+    # Desenhando Lutadores
+    lutador_1.draw(janela)
+    lutador_2.draw(janela)
+    
+
+    # Verificação da Derrota dos Jogadores
+    if RODADA == False:
+        if lutador_1.vivo == False:
+            PONTUAÇÃO[1] += 1
+            RODADA = True
+            RODADA_FIM = pygame.time.get_ticks()
+        elif lutador_2.vivo == False:
+            PONTUAÇÃO[0] += 1
+            RODADA = True
+            RODADA_FIM = pygame.time.get_ticks()        
+    else:
+        # Exibição da Imagem de Vitoria
+        janela.blit(vitoria_img, (360, 150))
+        if pygame.time.get_ticks() - RODADA_FIM > RODADA_NOVA:
+            RODADA = False
+            
+            INICIO_CONTADOR = 4
+            lutador_1 = Lutador(1, 200,310, False, NECROMANTE_DADOS, necromante_sheet, NECROMANTE_ANIMATIO_FRAME, magia_fx)
+            lutador_2 = Lutador(2, 700,310, True, CAVALEIRO_DADOS, cavaleiro_sheet, CAVALEIRO_ANIMATION_FRAME, espada_fx)
+    
+    
+    # Eventos...
+    for event in pygame.event.get():
+        # Botão para fechar
+        if event.type == pygame.QUIT:
+            ATIVO = False
+
+    # Atualização de Tela
+    pygame.display.update()
+
+# Fim de Jogo
+pygame.quit()
